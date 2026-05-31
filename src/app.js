20e1138cf2a163b27e4376d355fe1092636cb854
Constantine <13561194601@163.com>
2026-06-01 00:59:43 +0800

    Add HTML shell fallback for navigation

--- src/app.js
+++ src/app.js
@@ -1416,7 +1416,7 @@ function setRoute(viewId) {
   if (!isValidView(viewId)) return false;
   switchView(viewId);
   if (window.location.hash !== `#${viewId}`) {
-    window.history.replaceState(null, "", `#${viewId}`);
+    window.history.pushState(null, "", `#${viewId}`);
   }
   scrollToTop();
   return true;
